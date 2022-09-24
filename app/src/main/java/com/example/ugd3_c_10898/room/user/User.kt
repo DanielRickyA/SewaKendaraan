3a30e27373c9881eb257bcb3472e88f31af8cf95
PURELIGHT1 <purelight1709@gmail.com>
2022-09-24 22:12:49 +0700

    room user

---
+++ app/src/main/java/com/example/ugd3_c_10898/room/user/User.kt
@@ -0,0 +1,15 @@
+package com.example.ugd3_c_10898.room.user
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id : Int,
+    val username : String,
+    val password : String,
+    val email : String,
+    val tglLahir : String,
+    val noHp : String
+)
